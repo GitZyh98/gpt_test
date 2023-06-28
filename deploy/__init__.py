827d37681422367d147d8f5d1ce360604032d17a
GitZyh98 <68258272+GitZyh98@users.noreply.github.com>
2023-06-28 14:48:44 +0800

    Add files via upload

---
+++ deploy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2023/6/27 6:08 PM
+---------
+@summary: 
+---------
+@author: zhangyouhuang
+@email: 
+"""
---
+++ deploy/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='chat_gpt', packages=['chat_gpt'])
